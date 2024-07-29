dec77e8a64884dc9fa3acd60f9658bd9b8e1c156
Atharva Jakhetiya <124387682+Im-Arth1307@users.noreply.github.com>
2024-07-29 15:58:52 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+
+# model = tf.keras.Sequential([
+#     tf.keras.layers.Embedding(tokenizer.vocab_size, 64),
+#     tf.keras.layers.Biderectional(tf.keras.layers.LSTM(64)),
+#     tf.keras.layers.Dense(64, activation = 'relu'),
+#     tf.keras.layers.Dense(1, activation = 'sigmoid')
+# ])
